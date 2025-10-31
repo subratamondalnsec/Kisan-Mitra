8068ee0d1a4880180ae9f5f84537c3559a9c76f1
Rahul Khandait <rahulkhandait.cse2023@nsec.ac.in>
2025-11-01 04:53:53 +0530

    vite

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -32,5 +35,10 @@ export default defineConfig({
   preview: {
     port: 4173,
     strictPort: false,
-  }
-})
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
